741ceb60753b5a4fde5fef67a6a356985f9bcd27
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-04-11 22:30:09 +0200

    getting file size, a lot of fixes

--- lib/xmpp_chat_bot/base.rb
+++ lib/xmpp_chat_bot/base.rb
@@ -5,6 +5,9 @@ require 'blather/client/client'
 require 'eventmachine'
 
 require 'open-uri'
+require 'uri'
+require 'net/http'
+require 'net/https'
 
 require 'iconv'
 
@@ -18,7 +21,12 @@ module XmppChatBot
       @options = _options
 
       @options[:jid] = "#{@options[:login]}@#{@options[:server]}"
+      @options[:bot_chat_jid] = "#{@options[:room]}/#{@options[:bot_name]}"
+
       @url_regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+      @command_regexp = /#{@options[:bot_name]}:\s*(\w+)/
+
+      @iconv = ic_ignore = Iconv.new('UTF-8//IGNORE', 'UTF-8')
     end
 
 
@@ -44,12 +52,21 @@ module XmppChatBot
 
         p = Blather::Stanza::Presence.new
         p.from = @options[:jid]
-        p.to = "#{@options[:room]}/#{@options[:bot_name]}"
+        p.to = @options[:bot_chat_jid]
         p << "<x xmlns='http://jabber.org/protocol/muc'/>"
         @bc.write p
       end
 
-      # Echo back what was said
+      register_url_spy
+      register_simple_commands
+
+
+      EM.run { @bc.run }
+
+    end
+
+    # register handle for checking urls
+    def register_url_spy
       @bc.register_handler :message, :groupchat?, :body => @url_regexp do |m|
         if not m.from.to_s[/#{@options[:bot_name]}/]
           url = m.body.to_s[@url_regexp]
@@ -61,32 +78,96 @@ module XmppChatBot
           n.to = @options[:room]
           n.type = :groupchat
           #n.body = "#{m.from} added #{url}"
-          n.body = processed_url[:title].to_s
+          n.body = processed_url[:desc].to_s
           @bc.write n
         end
       end
-
-      EM.run { @bc.run }
-
     end
 
+    # register simple commands
+    def register_simple_commands
+      @bc.register_handler :message, :groupchat?, :body => @command_regexp do |m|
+        if m.body.to_s =~ @command_regexp
+          command = $1.to_s.strip
+          puts "command '#{command}'"
+
+          n = Blather::Stanza::Message.new
+          n.to = @options[:room]
+          n.type = :groupchat
+          n.body = "command #{command}\n" + process_command(command)
+          @bc.write n
+
+        end
+      end
+    end
+
+    def process_command(command)
+      return case command.to_s
+               when 'df' then
+                 `df -hl -x tmpfs`.to_s
+               when 'ps' then
+                 `ps -e -o pcpu,ruser,args|sort -nr|grep -v %CPU|head -10`
+               else
+                 'command not available'
+             end
+    end
+
+    # process single url
     def process_url(url)
-      resource = open(url)
-      str = resource.read(URL_OPEN_MAX_SIZE)
+      size = get_uri_size(url)
 
-      ic_ignore = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-      str = ic_ignore.iconv(str)
+      if size < URL_OPEN_MAX_SIZE
+        resource = open(url)
+        str = resource.read(URL_OPEN_MAX_SIZE)
+      else
+        str = ""
+      end
 
+      # final description
+      desc = ""
+
+      # image
+      if url =~ /(.+(jpg|png|gif|bmp))/i
+        puts "image #{url}"
+        desc = "image file size #{size}"
+      end
+
+      begin
+        str = @iconv.iconv(str)
+      rescue
+        # omg, it is not html
+      end
+
+      # searching for title
       title_regexp = /<title>([^<]*)<\/title>/i
-      title = str[title_regexp]
-      title ||= 'no title today, sorry :('
-      title.gsub!(/\s/, ' ')
+      title = 'no title today, sorry :('
+      begin
+        if str =~ title_regexp
+          title = $1.to_s
+          title = title.gsub(/\s/, ' ').strip
+          desc += title
+        end
+      rescue => e
+        puts e.inspect
+      end
 
-      puts title
-      
+      puts desc
+
       return {
-        :title => title
+        :title => title,
+        :size => size,
+        :desc => desc
       }
+    end
+
+    # hope it works
+    def get_uri_size(url)
+      uri = URI(url)
+      host = uri.host
+      path = uri.path
+
+      req = Net::HTTP.new(host, 80)
+      return req.request_head(path)['Content-Length'].to_i
     end
 
   end
